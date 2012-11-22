b01d6fd51bd1ac4088e8a5c2734acf965dc9ce65
Florian Hahn <flo@fhahn.com>
2012-11-22 18:20:51 +0100

    improved decToNat

--- Aufgabe6_test.hs
+++ Aufgabe6_test.hs
@@ -3,24 +3,24 @@ import Test.HUnit
 import Aufgabe6
 
 -- converts an integer to Nat
-decToNat :: Integer -> Nat -> Nat
-decToNat 0 val = val
-decToNat i val = decToNat (i-1) (S val)
+decToNat :: Integer -> Nat
+decToNat 0 = Z
+decToNat i = S (decToNat (i-1))
 
 
 -- tests cases for Aufgabe 6.1
-test8 = TestCase (assertEqual "" "VIII" (show (decToNat 8 Z)) )
-test9 = TestCase (assertEqual "" "IX" (show (decToNat 9 Z)))
-test19 = TestCase (assertEqual "" "XIX" (show (decToNat 19 Z)))
-test39 = TestCase (assertEqual "" "XXXIX"(show (decToNat 39 Z)))
-test84 = TestCase (assertEqual "" "LXXXIV" (show (decToNat 84 Z)))
-test89 = TestCase (assertEqual "" "LXXXIX" (show (decToNat 89 Z)))
-test98 = TestCase (assertEqual "" "XCVIII" (show (decToNat 98 Z)))
-test99 = TestCase (assertEqual "" "IC" (show (decToNat 99 Z)))
-test889 = TestCase (assertEqual "" "DCCCLXXXIX" (show (decToNat 889 Z)))
-test1899 = TestCase (assertEqual "" "MDCCCIC" (show (decToNat 1899 Z)))
-test4884 = TestCase (assertEqual "" "MMMMDCCCLXXXIV" (show (decToNat 4884 Z)))
-test5999 = TestCase (assertEqual "" "MMMMMIM" (show (decToNat 5999 Z)))
+test8 = TestCase (assertEqual "" "VIII" (show (decToNat 8)) )
+test9 = TestCase (assertEqual "" "IX" (show (decToNat 9)))
+test19 = TestCase (assertEqual "" "XIX" (show (decToNat 19)))
+test39 = TestCase (assertEqual "" "XXXIX"(show (decToNat 39)))
+test84 = TestCase (assertEqual "" "LXXXIV" (show (decToNat 84)))
+test89 = TestCase (assertEqual "" "LXXXIX" (show (decToNat 89)))
+test98 = TestCase (assertEqual "" "XCVIII" (show (decToNat 98)))
+test99 = TestCase (assertEqual "" "IC" (show (decToNat 99)))
+test889 = TestCase (assertEqual "" "DCCCLXXXIX" (show (decToNat 889)))
+test1899 = TestCase (assertEqual "" "MDCCCIC" (show (decToNat 1899)))
+test4884 = TestCase (assertEqual "" "MMMMDCCCLXXXIV" (show (decToNat 4884)))
+test5999 = TestCase (assertEqual "" "MMMMMIM" (show (decToNat 5999)))
 
 tests = TestList [TestLabel "Test 8" test8, TestLabel "Test 9" test9,
                   TestLabel "Test 19" test19, TestLabel "Test 39" test39,
