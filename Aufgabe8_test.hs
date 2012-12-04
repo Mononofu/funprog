aae78700afe2748a1f5cec1ab4eb87a1310e8131
Julian Schrittwieser <j.schrittwieser@gmail.com>
2012-12-04 23:15:03 +0100

    tests for isRoute

--- Aufgabe8_test.hs
+++ Aufgabe8_test.hs
@@ -14,10 +14,24 @@ test5 = TestCase (assertEqual "" (Success (0,989,"989")) (addRev 989 10))
 test6 = TestCase (assertEqual "" (Success (3,6666,"6666")) (addRev 750 (-5)))
 test7 = TestCase (assertEqual "" Failure (addRev 196 1000))
 
+
+connections = [(Air "Austria" "Germany" 2),
+               (Rail "Austria" "Germany" 10),
+               (Air "Germany" "Spain" 3),
+               (Sea "Spain" "UK" 15),
+               (Road "Canada" "US" 5)
+  ]
+
+test10 = TestCase (assertEqual "" True (isRoute connections "Austria" "Spain"))
+test11 = TestCase (assertEqual "" True (isRoute connections "Austria" "UK"))
+test12 = TestCase (assertEqual "" False (isRoute connections "US" "UK"))
+
 tests = TestList [TestLabel "Test 1" test1, TestLabel "Test 2" test2,
                   TestLabel "Test 3" test3, TestLabel "Test 4" test4,
                   TestLabel "Test 5" test5, TestLabel "Test 6" test6,
-                  TestLabel "rm_dup" test7]
+                  TestLabel "Test 7" test7,
+                  TestLabel "isRoute" test10, TestLabel "isRoute" test11,
+                  TestLabel "isRoute" test12]
 
 
 main = do
