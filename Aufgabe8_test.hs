e970611c63c859393abbf698601369a413944007
Julian Schrittwieser <j.schrittwieser@gmail.com>
2012-12-14 14:27:11 +0100

    added official tests for exercise 8

--- Aufgabe8_test.hs
+++ Aufgabe8_test.hs
@@ -42,6 +42,56 @@ test30 = TestCase (assertEqual "" 5 (getIternaryTime (yieldFastestRoute connecti
 test40 = TestCase (assertEqual "" True (isFeelGoodRoute connections "Austria" "Bulgaria" 0))
 test41 = TestCase (assertEqual "" False (isFeelGoodRoute connections "Austria" "UK" 0))
 
+test_off_addRev1 = TestCase (assertEqual ""
+  ( Success (0, 11, "11"), Failure)
+  ( addRev 11 100, addRev (-196)(-400)))
+
+test_off_addRev2 = TestCase (assertEqual ""
+  [196]
+  [i|i<-[1..196],Failure<-[addRev(-i)(-24)]])
+
+test_off_addRev3 = TestCase (assertEqual ""
+  [1,2,3,4,5,6,7,8,9,11,22,33,44,55,66,77,88,99]
+  [n|Success (_,n,_)<-[addRev(-i)0|i<-[1..100]]])
+
+test_off_addRev4 = TestCase (assertEqual ""
+  90
+  (length[n|Success (_,n,_)<-[addRev(-i)0|i<-[100..999]]]))
+
+test_off_isRoute =
+  let m=7; r=[1..m]
+      g=[Road(show i)(show j)1|i<-r,j<-[i+2],j<=m]
+  in TestCase (assertEqual ""
+  []
+  [(i,j)|i<-r,j<-r,i/=j,isRoute g(show i)(show j)/=((i+1)`mod`2/=j`mod`2)])
+
+test_off_yieldRoute =
+  let m=7; r=[1..m]
+      g=[Road(show i)(show j)1|i<-r,j<-[i+2],j<=m]
+  in TestCase (assertEqual ""
+  []
+  [(i,j)|i<-r,j<-r,i/=j,isRoute g(show i)(show j)/=(yieldRoute g(show i)(show j)/=[])])
+
+test_off_yieldFastestRoute =
+  let m=7; r=[1..m]
+      g=[Road(show i)(show j)1|i<-r,j<-[i+2],j<=m]
+  in TestCase (assertEqual ""
+  []
+  [(i,j)|i<-r,j<-r,i/=j,isRoute g(show i)(show j)/=(yieldFastestRoute g(show i)(show j)/=NoRoute)])
+
+test_off_isFeelGoodRoute =
+  let m=7; r=[1..m]
+      g=[Road(show i)(show j)1|i<-r,j<-[i+2],j<=m]
+  in TestCase (assertEqual ""
+  []
+  [(i,j)|i<-r,j<-r,i/=j,isRoute g(show i)(show j)/=isFeelGoodRoute g(show i)(show j)m])
+
+test_off_yieldFeelGoodRoute =
+  let m=7; r=[1..m]
+      g=[Road(show i)(show j)1|i<-r,j<-[i+2],j<=m]
+  in TestCase (assertEqual ""
+  []
+  [(i,j)|i<-r,j<-r,i/=j,isRoute g(show i)(show j)/=(yieldFeelGoodRoute g(show i)(show j)m/=NoRoute)])
 
 tests = TestList [TestLabel "Test 1" test1, TestLabel "Test 2" test2,
                   TestLabel "Test 3" test3, TestLabel "Test 4" test4,
@@ -51,7 +101,16 @@ tests = TestList [TestLabel "Test 1" test1, TestLabel "Test 2" test2,
                   TestLabel "isRoute" test12, TestLabel "isRoute" test13,
                   TestLabel "yieldRoute" test20,
                   TestLabel "yieldFastestRoute" test30,
-                  TestLabel "isFeelGoodRoute" test40, TestLabel "isFeelGoodRoute" test41]
+                  TestLabel "isFeelGoodRoute" test40, TestLabel "isFeelGoodRoute" test41,
+                  TestLabel "test_off_addRev1" test_off_addRev1,
+                  TestLabel "test_off_addRev2" test_off_addRev2,
+                  TestLabel "test_off_addRev3" test_off_addRev3,
+                  TestLabel "test_off_addRev4" test_off_addRev4,
+                  TestLabel "test_off_isRoute" test_off_isRoute,
+                  TestLabel "test_off_yieldRoute" test_off_yieldRoute,
+                  TestLabel "test_off_yieldFastestRoute" test_off_yieldFastestRoute,
+                  TestLabel "test_off_isFeelGoodRoute" test_off_isFeelGoodRoute,
+                  TestLabel "test_off_yieldFeelGoodRoute" test_off_yieldFeelGoodRoute]
 
 
 main = do
