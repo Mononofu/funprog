110488ab1b4c07e3d369833ef4b2642f9fc7d9b3
Julian Schrittwieser <j.schrittwieser@gmail.com>
2012-12-14 15:09:04 +0100

    more tests for lucky numbers

--- Aufgabe9_test.hs
+++ Aufgabe9_test.hs
@@ -34,13 +34,33 @@ completeConnections = [
 getIternaryTime (Route (_, time)) = time
 getIternaryTime _ = 0
 
+-- the first 22 lucky numbers, according to wikipedia
+lucky_numbers_22 = [3, 7, 9, 13, 15, 21, 25, 31, 33, 37, 43, 49, 51, 63, 67, 69, 73, 75, 79, 87, 93, 99]
 
-test_luckynums_take5 = TestCase (assertEqual ""
-  [3, 7, 9, 13, 15, 21, 25, 31, 33, 37, 43, 49, 51, 63, 67, 69, 73, 75, 79, 87, 93, 99]
+test_luckynums_take22 = TestCase (assertEqual ""
+  lucky_numbers_22
   (take 22 luckyNumbers))
 
+test_luckynums_isLuckyNumber = TestCase (assertEqual ""
+  lucky_numbers_22
+  [n | n <- [1..100], isLuckyNumber n])
+
+test_luckynums_yieldLuckyNumbers = TestCase (assertEqual ""
+  lucky_numbers_22
+  (yieldLuckyNumbers 1 100))
+
+test_luckynums_isTwinLuckyNumber = TestCase (assertEqual ""
+  [isTwinLuckyNumber n | n <- [1..98]]
+  ([if n `elem` lucky_numbers_22 then
+          (if (n+2) `elem` lucky_numbers_22 || (n-2) `elem` lucky_numbers_22 then True
+           else False)
+        else False | n <- [1..98]]))
+
 tests = TestList [
-  TestLabel "luckyNumbers take 5" test_luckynums_take5]
+  TestLabel "luckyNumbers take 22" test_luckynums_take22,
+  TestLabel "isLuckyNumbers" test_luckynums_isLuckyNumber,
+  TestLabel "test_luckynums_yieldLuckyNumbers" test_luckynums_yieldLuckyNumbers,
+  TestLabel "test_luckynums_isTwinLuckyNumber" test_luckynums_isTwinLuckyNumber]
 
 prop_yieldLuckyNumbers m n
   | m > n     = nums == []
